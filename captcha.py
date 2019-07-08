765d56ede1ba05c9b9d6189ec32f7592edcf817c
kiiiiiingdom <43037179+kiiiiiingdom@users.noreply.github.com>
2019-07-08 15:09:02 +0930

    finish
    yeeeeeeeeee

--- captcha.py
+++ captcha.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 
 import os
@@ -16,51 +17,42 @@ URLPOST = "http://43.241.202.33:3000/api/Feedbacks/"
 def captchasolver(mathString):
 	return eval(mathString)
 
-#print("1")
-
-for i in range(0,6):
-	#print("2")
+for i in range(0,50):
 	p = requests.get(url = URLGET)
-	print(p.text)
 	captcha_id = p.text.split("\"")[2]
 	captcha_id = captcha_id[:-1][1:]
 	#captcha id
 	captcha_string = p.text.split("\"")[5]
 	#math string
 
-	print("Return Data = ", BLUE + p.text + ENDC)
-	print("Captcha id = ",BLUE + captcha_id + ENDC)
-	print("Math string = ",BLUE + captcha_string + " ,")
+	print("Return Data = " + p.text)
+	print("Captcha id = " + captcha_id)
+	print("Math string = " + captcha_string)
 	
 	captcha_answer = captchasolver(captcha_string)
-	print("Answer = ",captcha_answer)
+	print("Answer = " + str(captcha_answer))
 
-	a = '''
-POST /api/Feedbacks/ HTTP/1.1
-Host: 43.241.202.33:3000
-User-Agent: Mozilla/5.0 (X11; Linux i686; rv:60.0) Gecko/20100101 Firefox/60.0
-Accept: application/json, text/plain, */*
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate
-Referer: http://43.241.202.33:3000/
-Content-Type: application/json;charset=utf-8
-Authorization: Bearer 
-eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGF0dXMiOiJzdWNjZXNzIiwiZGF0YSI6eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBrd2lrZS5tYXJ0IiwicGFzc3dvcmQiOiI3YzZhMTgwYjM2ODk2YTBhOGMwMjc4N2VlYWZiMGU0YyIsImNyZWF0ZWRBdCI6IjIwMTktMDYtMjVUMTE6MjI6MzUuMjk4WiIsInVwZGF0ZWRBdCI6IjIwMTktMDYtMjVUMTE6MjI6MzUuMjk4WiJ9LCJpYXQiOjE1NjI1MDk3NzcsImV4cCI6MTU2MjUyNzc3N30.Bqo-NG_YJ1hfqq35gKIaaCy64VmGVNtHv8ji_2aE6bvofwqi_4rXo8H_CcLzuvOe2P2DG_fejnLhOuVH-dtlS90QdvhxsZuLt8X3CvPy5WxdDXmnYRKBscwKdfoCF2zVJCNqhbkglIJslCZXtgTL9jeQzNjNJbjv2Zk0I5s4ogA
-Content-Length: 69
-Cookie: cookieconsent_status=dismiss;
-continueCode=VoLJZm25zaE8lXOPWKAorHNtWcXIETlCLsKfguocL0ybn1M47QNkYBvDxqjr; 
-io=1pUQu0B_8yP0v0fhAAzH; language=en; 
-token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGF0dXMiOiJzdWNjZXNzIiwiZGF0YSI6eyJpZCI6MSwiZW1haWwiOiJhZG1pbkBrd2lrZS5tYXJ0IiwicGFzc3dvcmQiOiI3YzZhMTgwYjM2ODk2YTBhOGMwMjc4N2VlYWZiMGU0YyIsImNyZWF0ZWRBdCI6IjIwMTktMDYtMjVUMTE6MjI6MzUuMjk4WiIsInVwZGF0ZWRBdCI6IjIwMTktMDYtMjVUMTE6MjI6MzUuMjk4WiJ9LCJpYXQiOjE1NjI1MDk3NzcsImV4cCI6MTU2MjUyNzc3N30.Bqo-NG_YJ1hfqq35gKIaaCy64VmGVNtHv8ji_2aE6bvofwqi_4rXo8H_CcLzuvOe2P2DG_fejnLhOuVH-dtlS90QdvhxsZuLt8X3CvPy5WxdDXmnYRKBscwKdfoCF2zVJCNqhbkglIJslCZXtgTL9jeQzNjNJbjv2Zk0I5s4ogA
-Connection: close
+	headers = {
+		'Host': '43.241.202.33:3000',
+		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0',
+		'Accept': 'application/json, text/plain, */*',
+		'Accept-Language': 'en-US,en;q=0.5',
+		'Accept-Encoding': 'gzip, deflate',
+		'Referer': 'http://43.241.202.33:3000/administration',
+		'Content-Type': 'application/json;charset=utf-8',
+		'Content-Length': '56',
+		'Cookie': 'cookieconsent_status=dismiss;continueCode=mnWyjRDqM8OVJE1Z5e4r9bXpaAP9HPCysld2kBLm3KwlNx6YnWgoP7zvQeRE;io=q-wn9uU_bfTf1ZCKAA1B',
+		'Connection': 'keep-alive'
+		}
 
-{"UserId":1,"comment":"asd","rating":1,"captcha":"'''
+	a1 = '''{"UserId":1,"comment":"asd","rating":1,"captcha":"'''
 
 	b = str(captcha_answer)
 	c ='''","captchaId":'''
 
-	d = a +b+ c + captcha_id + "}"
-	#print("Posting: ", RED + d + ENDC + BLUE)
-	enn = requests.post(url = URLPOST,data = d)
-	print(enn.text) 
-
-
+	d = a1 + b+ c + str(captcha_id) + "}"
+	#d = '''{"comment":"1","rating":1,"captcha":"''' + b + '''","captchaId":''' + "430" + '}'
+	print("Posting: " + d)
+	#os.system(d)
+	enn = requests.post(URLPOST,data = d,headers = headers)
+	print(enn.text + "..............\n") 
