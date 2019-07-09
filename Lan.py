4a8d04e9a52978d1ee6ed724aacc25ef20db6283
kiiiiiingdom <43037179+kiiiiiingdom@users.noreply.github.com>
2019-07-09 11:29:06 +0930

    final version

--- Lan.py
+++ Lan.py
@@ -6,7 +6,6 @@ import glob
 import sys
 import string
 
-
 BLUE = '\033[94m'
 RED = '\033[91m'
 ENDC = '\033[0m'
@@ -15,19 +14,16 @@ ENDC = '\033[0m'
 def requesting(namestring):
 	try:
 		p = requests.get("http://43.241.202.33:3000/i18n/" + namestring + ".json")
-		if "<!DOCTYPE html>" in p.text:
+		if "<!DOCTYPE html>"not in p.text:
 		#means theres a page exist
 			#print(p.text)
-			#f = open(namestring + ".json", 'w')
-			#f.write(p.text)
-			#f,close();
+			f = open(namestring + ".json", 'w')
+			f.write(p.text)
+			f,close();
 			print(BLUE + string)
 	except:
 		print(RED + namestring + ENDC)
 
-requesting('it_LT')
-requesting('it_LLL')
-'''
 for c in string.ascii_lowercase:
 	for d in string.ascii_lowercase:
 		for f in string.ascii_uppercase:
@@ -36,4 +32,3 @@ for c in string.ascii_lowercase:
 				#print("Testing: " + namestring + ENDC)
 				requesting(namestring)
 
-'''
